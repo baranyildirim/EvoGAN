a8656d6f7db8e868f01acc772ab262b33d76663a
Baran Yildirim <baran.yildirim@outlook.com>
2020-10-28 18:28:21 -0400

    More file placeholders

--- run_unittest.py
+++ run_unittest.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
+
 from tests import parameters_test
 import unittest
 
-unittest.main(parameters_test)
+
+def main():
+    unittest.main(parameters_test)
+
+if __name__ == "__main__":
+    main()
+
