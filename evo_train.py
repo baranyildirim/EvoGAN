80392bc6add18cf43d55093167e43b39fbf14ec4
Baran Yildirim <baran.yildirim@outlook.com>
2020-11-06 02:29:09 -0500

    Rewriting training loop

--- evo_train.py
+++ evo_train.py
@@ -13,10 +13,10 @@ def generate_new_dna(n_dna:int, properties: DNAProperties) -> List[DNA]:
 # This will create GAN from dna
 # train GAN and
 # return Inception score
-def score_dna(dna):
+def score_dna(dna: DNA) -> float:
     # This is the target dna
     target_dna = np.array([1, 0, 0, 1, 0])
-
+    dna = np.array(dna.serialize())
     # This is the reward for matching each position
     reward_dict = np.array([3.0, 8.0, 4.0, 2.0, 5.0])
 
@@ -25,27 +25,10 @@ def score_dna(dna):
 
     # Compute rewards for dna
     rewards = np.sum(matches * reward_dict, axis=1)
-
     return rewards
 
 
-def generation_step(dna, scores, properties: DNAProperties):
-
-    n_dna, dna_length = dna.shape
-
-    p_dist = np.zeros((dna_length, 2))
-
-    # Compute probabilities using scores
-
-    for n in range(dna_length):
-        p0 = np.sum(scores[dna[:, n] == 0])
-        p1 = np.sum(scores[dna[:, n] == 1])
-        z = p0 + p1
-        p0 /= z
-        p1 /= z
-
-        p_dist[n, 0] = p0
-        p_dist[n, 1] = p1
+def generation_step(dna: List[DNA], scores: List[List[float]], properties: DNAProperties) -> List[DNA]:
 
     print(p_dist)
     dna = generate_new_dna(n_dna, properties)
@@ -56,17 +39,15 @@ def generation_step(dna, scores, properties: DNAProperties):
 
 
 def main():
-    # Initialize dna uniformly
-    dna = generate_new_dna(10)
-
     # Initialize mutation probability
     properties = DNAProperties(mutation_probability=0.05)
 
+    # Initialize dna uniformly
+    dna = generate_new_dna(10, properties)
     n_epochs = 20
 
     for epoch in range(n_epochs):
-        rewards = score_dna(dna)
-        print(dna, rewards)
+        rewards = score_dna(dna)    
         dna = generation_step(dna, rewards, properties)
 
         if epoch % 2 == 0:
